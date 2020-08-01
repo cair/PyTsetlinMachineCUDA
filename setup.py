5579fa51af4f867f811f99ebb063b9b6f3d4aeb2
Ole-Christoffer Granmo <ole.granmo@uia.no>
2020-08-01 09:49:32 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import *
+
+setup(
+   name='pyTsetlinMachineCUDA',
+   version='0.1.1',
+   author='Ole-Christoffer Granmo',
+   author_email='ole.granmo@uia.no',
+   url='https://github.com/cair/pyTsetlinMachineCUDA/',
+   license='MIT',
+   description='A Massively Parallel and Asynchronous Architecture for Logic-based AI.',
+   long_description='A Massively Parallel and Asynchronous Architecture for Logic-based AI. Implements the Tsetlin Machine, Convolutional Tsetlin Machine, Regression Tsetlin Machine, and Weighted Tsetlin Machine, with support for continuous features.',
+   keywords ='pattern-recognition cuda machine-learning interpretable-machine-learning rule-based-machine-learning propositional-logic tsetlin-machine regression convolution',
+   packages=['pyTsetlinMachineCUDA']
+)
