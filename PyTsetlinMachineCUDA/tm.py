d02c9d5b55be028b518ecbc243ca7c42bd3355b5
Ole-Christoffer Granmo <ole.granmo@uia.no>
2020-08-01 09:58:16 +0200

    Update tm.py

--- PyTsetlinMachineCUDA/tm.py
+++ PyTsetlinMachineCUDA/tm.py
@@ -23,7 +23,7 @@
 
 import numpy as np
 
-import pyTsetlinMachineCUDA.kernels as kernels
+import PyTsetlinMachineCUDA.kernels as kernels
 
 import pycuda.curandom as curandom
 import pycuda.driver as cuda
