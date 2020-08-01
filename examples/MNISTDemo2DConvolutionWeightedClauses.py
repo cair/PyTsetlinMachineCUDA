fabcf528d80d68814f7954e85932a4f46659cee2
Ole-Christoffer Granmo <ole.granmo@uia.no>
2020-08-01 10:11:31 +0200

    Add files via upload

---
+++ examples/MNISTDemo2DConvolutionWeightedClauses.py
@@ -0,0 +1,25 @@
+from PyTsetlinMachineCUDA.tm import MultiClassConvolutionalTsetlinMachine2D
+
+import numpy as np
+from time import time
+
+from keras.datasets import mnist
+
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+
+X_train = np.where(X_train >= 75, 1, 0) 
+X_test = np.where(X_test >= 75, 1, 0) 
+
+tm = MultiClassConvolutionalTsetlinMachine2D(2000, 50*15, 5.0, (10, 10), number_of_state_bits=10, max_weight=16)
+
+print("\nAccuracy over 250 epochs:\n")
+for i in range(250):
+    start_training = time()
+    tm.fit(X_train, Y_train, epochs=1, incremental=True)
+    stop_training = time()
+
+    start_test = time()
+    result = 100*(tm.predict(X_test) == Y_test).mean()
+    stop_test = time()
+
+    print("#%d Accuracy: %.2f%% Training: %.2fs Testing: %.2fs" % (i+1, result, stop_training-start_training, stop_testing-start_testing))
