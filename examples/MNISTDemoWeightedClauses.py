ecbd04829bd9915a917b5613bfdb3c56a1d55928
Ole-Christoffer Granmo <ole.granmo@uia.no>
2020-08-02 07:33:36 +0200

    Update MNISTDemoWeightedClauses.py

--- examples/MNISTDemoWeightedClauses.py
+++ examples/MNISTDemoWeightedClauses.py
@@ -9,7 +9,7 @@ from keras.datasets import mnist
 X_train = np.where(X_train.reshape((X_train.shape[0], 28*28)) > 75, 1, 0) 
 X_test = np.where(X_test.reshape((X_test.shape[0], 28*28)) > 75, 1, 0) 
 
-tm = MultiClassTsetlinMachine(2000, 50*100, 10.0, max_weight=255)
+tm = MultiClassTsetlinMachine(2000, 50*16, 10.0, max_weight=16)
 
 print("\nAccuracy over 100 epochs:\n")
 for i in range(100):
