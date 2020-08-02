2998a34368a0c32cbaa522c9bdd930a869252939
Ole-Christoffer Granmo <ole.granmo@uia.no>
2020-08-02 05:49:24 +0200

    Create FashionMNISTDemo2DConvolutionWeightedClauses.py

---
+++ examples/FashionMNISTDemo2DConvolutionWeightedClauses.py
@@ -0,0 +1,29 @@
+from PyTsetlinMachineCUDA.tm import MultiClassConvolutionalTsetlinMachine2D
+import numpy as np
+from time import time
+import cv2
+from keras.datasets import fashion_mnist
+
+(X_train, Y_train), (X_test, Y_test) = fashion_mnist.load_data()
+X_train = np.copy(X_train)
+X_test = np.copy(X_test)
+
+for i in range(X_train.shape[0]):
+	X_train[i,:] = cv2.adaptiveThreshold(X_train[i], 1, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+
+for i in range(X_test.shape[0]):
+	X_test[i,:] = cv2.adaptiveThreshold(X_test[i], 1, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+
+tm = MultiClassConvolutionalTsetlinMachine2D(2000, 50*100, 5.0, (10, 10), max_weight=255)
+
+print("\nAccuracy over 30 epochs:\n")
+for i in range(30):
+    start_training = time()
+    tm.fit(X_train, Y_train, epochs=1, incremental=True)
+    stop_training = time()
+
+    start_testing = time()
+    result = 100*(tm.predict(X_test) == Y_test).mean()
+    stop_testing = time()
+
+    print("#%d Accuracy: %.2f%% Training: %.2fs Testing: %.2fs" % (i+1, result, stop_training-start_training, stop_testing-start_testing))
