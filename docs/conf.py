fbbfbdca9c469e188dae97acf89f5043f650b5ef
Ole-Christoffer Granmo <ole.granmo@uia.no>
2020-08-13 22:25:00 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -20,8 +20,12 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-import sphinx_rtd_theme
+import mock
+MOCK_MODULES = ['pycuda']
+for mod_name in MOCK_MODULES:
+sys.modules[mod_name] = mock.Mock()
 
+import sphinx_rtd_theme
 
 # -- General configuration ------------------------------------------------
 
